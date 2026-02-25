0e47b64b075a38b23788fe3ccadb8c85c9998e5d
Daniel Dias <danynovaisdias@outlook.com>
2026-02-24 21:15:28 -0300

    stuff

--- src/chip8/chip8.c
+++ src/chip8/chip8.c
@@ -7,65 +7,73 @@
 
 typedef struct CHIP8 {
 
-  uint8_t memory[MEMORY_SIZE];
-  uint8_t v_register[16];
+    uint8_t memory[MEMORY_SIZE];
+    uint8_t v_register[16];
 
-  uint8_t key[16];
+    uint8_t key[16];
 
-  uint8_t gfx[64 * 32];
-  uint8_t delay_timer;
-  uint8_t sound_timer;
+    uint8_t gfx[64 * 32];
+    uint8_t delay_timer;
+    uint8_t sound_timer;
 
-  uint16_t opcode;
-  uint16_t idx_register;
-  uint16_t pc_counter;
+    uint16_t opcode;
+    uint16_t idx_register;
+    uint16_t pc_counter;
 
-  uint16_t stack[16];
-  uint16_t stack_pointer;
+    uint16_t stack[16];
+    uint16_t stack_pointer;
 
 } CHIP8;
 
 CHIP8 EmulatorState = {0};
 int RomSize = 0;
 
-uint16_t fetchNextInstruction() {
-  if (EmulatorState.pc_counter + 1 > MEMORY_SIZE) {
-    return 0;
-  }
+void FetchNextInstruction(uint8_t InstructionArr[2]) {
+    if (EmulatorState.pc_counter + 1 > MEMORY_SIZE) {
+        return;
+    }
 
-  uint8_t byte1 = EmulatorState.memory[EmulatorState.pc_counter];
-  uint8_t byte2 = EmulatorState.memory[EmulatorState.pc_counter + 1];
-  uint16_t nextInstruction = (byte1 << 8) | byte2;
+    uint8_t byte1 = EmulatorState.memory[EmulatorState.pc_counter];
+    uint8_t byte2 = EmulatorState.memory[EmulatorState.pc_counter + 1];
+    // uint16_t nextInstruction = (byte1 << 8) | byte2;
 
-  EmulatorState.pc_counter += 2;
+    InstructionArr[0] = byte1;
+    InstructionArr[1] = byte2;
 
-  return nextInstruction;
+    EmulatorState.pc_counter += 2;
+
+    return;
 }
 
-int LoadGameIntoMemory(const char *fileName) {
-  unsigned char *fileData = LoadFileData(fileName, &RomSize);
+int LoadGameIntoMemory(const char* fileName) {
+    unsigned char* fileData = LoadFileData(fileName, &RomSize);
 
-  if (fileData == NULL) {
-    return 1;
-  }
+    if (fileData == NULL) {
+        return 1;
+    }
 
-  // maybe check if romsize + 512 is bigger than memory.
+    // maybe check if romsize + 512 is bigger than memory.
 
-  memcpy(EmulatorState.memory + 512, fileData, RomSize);
+    memcpy(EmulatorState.memory + 512, fileData, RomSize);
 
-  EmulatorState.pc_counter = 512;
+    EmulatorState.pc_counter = 512;
 
-  UnloadFileData(fileData);
+    UnloadFileData(fileData);
 
-  return 0;
+    return 0;
 }
 
 void SimulateCycle() {
-  uint16_t nextInstruction = fetchNextInstruction();
+    uint8_t NextInstruction[2] = {0};
+    FetchNextInstruction(NextInstruction);
 
-  if (nextInstruction == 0) {
-    return;
-  }
+    if (NextInstruction == 0) {
+        return;
+    }
 
-  printf("%x", nextInstruction);
+    printf("first byte: %x, second byte: %x\n ", NextInstruction[0], NextInstruction[1]);
+
+    // DecodeInstruction();
+
+    // ExecuteInstruction();
 }
--- src/main.c
+++ src/main.c
@@ -1,40 +1,56 @@
+#include "chip8.h"
+#include "resource_dir.h"
 #include <raylib.h>
 #include <stddef.h>
-#include "resource_dir.h"
-#include "chip8.h"
 
 #define WIDTH 1280
 #define HEIGHT 800
 #define PROJNAME "CHIP-8 Emulator"
 #define RESOURCES_DIR "resources"
 
-int main()
-{
+typedef enum {
+    RUN_MODE_NORMAL,
+    RUN_MODE_STEP,
+} RUN_MODE;
 
-	SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
-	InitWindow(WIDTH, HEIGHT, PROJNAME);
-	SetTargetFPS(60);
-	SearchAndSetResourceDir(RESOURCES_DIR);
+RUN_MODE CurrentRunMode = RUN_MODE_STEP;
 
-	int success = LoadGameIntoMemory("test_opcode.ch8");
+void StepCycle() {
+    switch (CurrentRunMode) {
+        case RUN_MODE_NORMAL:
+            SimulateCycle();
+            break;
+        case RUN_MODE_STEP:
+            bool HasPressedStepKey = IsKeyPressed(KEY_K);
+            if (HasPressedStepKey)
+                SimulateCycle();
+    }
+}
 
-	// Failed to load rom file.
-	if (success == 1)
-	{
-		return 1;
-	}
+int main() {
 
-	while (!WindowShouldClose())
-	{
-		BeginDrawing();
+    SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
+    InitWindow(WIDTH, HEIGHT, PROJNAME);
+    SetTargetFPS(60);
+    SearchAndSetResourceDir(RESOURCES_DIR);
 
-		SimulateCycle();
+    int success = LoadGameIntoMemory("roms/IBMLogo.ch8");
 
-		ClearBackground(BLACK);
+    // Failed to load rom file.
+    if (success == 1) {
+        return 1;
+    }
 
-		EndDrawing();
-	}
+    while (!WindowShouldClose()) {
+        BeginDrawing();
 
-	CloseWindow();
-	return 0;
+        StepCycle();
+
+        ClearBackground(BLACK);
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
 }
